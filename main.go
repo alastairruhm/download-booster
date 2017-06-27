f996800d08562e16e588b4676ea0e9d3fa60ae05
alastairruhm <alastairruhm@gmail.com>
2017-06-27 17:34:12 +0800

    feat: emit requests with proxy
    using gorequest package with proxy list pre-defined

--- main.go
+++ main.go
@@ -1,74 +1,43 @@
 package main
 
 import (
-	"io/ioutil"
-	"log"
-	"mime"
-	"net/http"
+	"fmt"
 	"os"
-	"strconv"
-	"sync"
+
+	"github.com/alastairruhm/download-booster/proxy"
+	"github.com/urfave/cli"
 )
 
-type Get struct {
-	Url           string
-	Header        http.Header
-	MediaType     string
-	MediaParams   map[string]string
-	ContentLength int
-	GetClient     http.Client
-	WG            sync.WaitGroup
-}
-
-// var wg sync.WaitGroup
-
 func main() {
-	var get Get
-	get.Url = "http://7b1h1l.com1.z0.glb.clouddn.com/bryce.jpg"
-	res, _ := http.Head(get.Url) // 187 MB file of random numbers per line
+	app := cli.NewApp()
+	app.Name = "download-boost"
+	app.Usage = "boost download in internal network"
 
-	get.Header = res.Header
-	get.ContentLength = int(res.ContentLength)
-	get.MediaType, get.MediaParams, _ = mime.ParseMediaType(res.Header.Get("Content-Disposition"))
-	log.Printf("Get %s MediaType:%s, Filename:%s, Length %d.\n", get.Url, get.MediaType, get.MediaParams["filename"], get.ContentLength)
+	var get proxy.Get
 
-	if get.Header.Get("Accept-Ranges") != "" {
-		log.Printf("Server %s support Range by %s.\n", get.Header.Get("Server"), get.Header.Get("Accept-Ranges"))
-	} else {
-		log.Printf("Server %s doesn't support Range.\n", get.Header.Get("Server"))
-		os.Exit(-1)
-	}
-
-	length, _ := strconv.Atoi(get.Header["Content-Length"][0]) // Get the content length from the header request
-	limit := 2                                                 // 10 Go-routines for the process so each downloads 18.7MB
-	lenSub := length / limit                                   // Bytes for each Go-routine
-	diff := length % limit                                     // Get the remaining for the last request
-	body := make([]string, 11)                                 // Make up a temporary array to hold the data to be written to the file
-	for i := 0; i < limit; i++ {
-		get.WG.Add(1)
-
-		min := lenSub * i       // Min range
-		max := lenSub * (i + 1) // Max range
-
-		if i == limit-1 {
-			max += diff // Add the remaining bytes in the last request
+	app.Before = func(c *cli.Context) error {
+		// check url validation
+		// fmt.Println(c.Args().First())
+		// check range support
+		supportRange, err := get.CheckAcceptRangeSupport(c.Args().First())
+		if err != nil {
+			return err
 		}
-
-		go func(min int, max int, i int) {
-			client := &http.Client{}
-			// 在此处分配代理池
-			req, _ := http.NewRequest("GET", get.Url, nil)
-			rangeHeader := "bytes=" + strconv.Itoa(min) + "-" + strconv.Itoa(max-1) // Add the data for the Range header of the form "bytes=0-100"
-			req.Header.Add("Range", rangeHeader)
-			resp, _ := client.Do(req)
-			defer resp.Body.Close()
-
-			reader, _ := ioutil.ReadAll(resp.Body)
-			body[i] = string(reader)
-			ioutil.WriteFile(strconv.Itoa(i), []byte(string(body[i])), 0x777) // Write to the file i as a byte array
-			get.WG.Done()
-			// ioutil.WriteFile("new_oct.png", []byte(string(body)), 0x777)
-		}(min, max, i)
+		if supportRange == false {
+			fmt.Printf("Server %s doesn't support Range.\n", get.Header.Get("Server"))
+			os.Exit(-1)
+		}
+		fmt.Printf("Server %s support Range by %s.\n", get.Header.Get("Server"), get.Header.Get("Accept-Ranges"))
+		return nil
 	}
-	get.WG.Wait()
+
+	app.Action = func(c *cli.Context) error {
+		err := get.DownloadInParallel(c.Args().First())
+		if err != nil {
+			return err
+		}
+		return nil
+	}
+
+	app.Run(os.Args)
 }
---
+++ proxy/proxy.go
@@ -0,0 +1,121 @@
+package proxy
+
+import (
+	"fmt"
+	"io/ioutil"
+	"mime"
+	"net/http"
+	"strconv"
+	"sync"
+
+	"strings"
+
+	"github.com/parnurzeal/gorequest"
+)
+
+// Get is getter with metadata
+type Get struct {
+	Url             string
+	Header          http.Header
+	MediaType       string
+	MediaParams     map[string]string
+	ContentLength   int
+	GetClient       http.Client
+	HeadRequestDone bool
+	FileName        string
+	WG              sync.WaitGroup
+}
+
+func GetFileName(url string) string {
+	routeSlice := strings.Split(url, "/")
+	fileName := routeSlice[len(routeSlice)-1]
+	if strings.Index(fileName, "?") != -1 {
+		fileName = fileName[0:strings.Index(fileName, "?")]
+	}
+	return fileName
+}
+
+func (get *Get) CheckAcceptRangeSupport(url string) (bool, error) {
+	get.Url = url
+	res, err := http.Head(get.Url)
+	if err != nil {
+		return false, err
+	}
+	fmt.Printf("%+v", res)
+	if res.StatusCode == 301 || res.StatusCode == 302 {
+		finalURL := res.Request.URL.String()
+		fmt.Printf("The URL you ended up at is: %v\n", finalURL)
+	}
+	get.FileName = GetFileName(url)
+
+	get.Header = res.Header
+	get.ContentLength = int(res.ContentLength)
+	get.MediaType, get.MediaParams, _ = mime.ParseMediaType(res.Header.Get("Content-Disposition"))
+	fmt.Printf("Get %s MediaType:%s, Filename:%s, Length %d.\n", get.Url, get.MediaType, get.MediaParams["filename"], get.ContentLength)
+	if get.MediaParams["filename"] != "" {
+		get.FileName = get.MediaParams["filename"]
+	}
+	get.HeadRequestDone = true
+	if get.Header.Get("Accept-Ranges") != "" {
+		fmt.Printf("Server %s support Range by %s.\n", get.Header.Get("Server"), get.Header.Get("Accept-Ranges"))
+		return true, nil
+	}
+	fmt.Printf("Server %s doesn't support Range.\n", get.Header.Get("Server"))
+	return false, nil
+}
+
+func (get *Get) DownloadInParallel(url string) error {
+	proxyList := [...]string{
+		"http://10.34.50.246:1080",
+		"http://10.34.50.247:1080",
+		"http://10.34.50.248:1080",
+		"http://10.34.50.187:1080",
+	}
+
+	if !get.HeadRequestDone {
+		_, err := get.CheckAcceptRangeSupport(url)
+		if err != nil {
+			return nil
+		}
+	}
+
+	length, _ := strconv.Atoi(get.Header["Content-Length"][0]) // Get the content length from the header request
+	limit := len(proxyList)                                    // 10 Go-routines for the process so each downloads 18.7MB
+	lenSub := length / limit                                   // Bytes for each Go-routine
+	diff := length % limit                                     // Get the remaining for the last request
+	// body := make([]string, 11)                                 // Make up a temporary array to hold the data to be written to the file
+	for i := 0; i < limit; i++ {
+		get.WG.Add(1)
+
+		min := lenSub * i       // Min range
+		max := lenSub * (i + 1) // Max range
+
+		if i == limit-1 {
+			max += diff // Add the remaining bytes in the last request
+		}
+
+		go func(min int, max int, i int) {
+			// client := &http.Client{}
+			// 在此处分配代理池
+			// req, _ := http.NewRequest("GET", get.Url, nil)
+			req := gorequest.New().Proxy(proxyList[i])
+			rangeHeader := "bytes=" + strconv.Itoa(min) + "-" + strconv.Itoa(max-1) // Add the data for the Range header of the form "bytes=0-100"
+			// req.Header.Add("Range", rangeHeader)
+			// resp, _ := client.Do(req)
+			_, bodyBytes, _ := req.Get(get.Url).
+				Set("Range", rangeHeader).
+				EndBytes()
+			// defer resp.Body.Close()
+
+			// reader, _ := ioutil.ReadAll(resp.Body)
+			// body[i] = string(reader)
+
+			tempFileSegName := get.FileName + "." + strconv.Itoa(i)
+			ioutil.WriteFile(tempFileSegName, bodyBytes, 0x777) // Write to the file i as a byte array
+			get.WG.Done()
+			// ioutil.WriteFile("new_oct.png", []byte(string(body)), 0x777)
+		}(min, max, i)
+	}
+	get.WG.Wait()
+	return nil
+}
